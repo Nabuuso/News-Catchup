eab954641e2e862d53475eb84b57af76f469301b
betty <betty.nabuuso@moringaschool.com>
2021-11-09 18:05:23 +0300

    running the application

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 class Config:
 
     
-    # NEWS_API_BASE_URL = 'https://newsapi.org/v2/sources?apiKey={}'
+   
     NEWS_API_BASE_URL = 'https://newsapi.org/v2/sources?language=en&apiKey={}'
     NEWS_API_ARTICLES_URL = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
     #NEWS_API_ARTICLES_URL = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
