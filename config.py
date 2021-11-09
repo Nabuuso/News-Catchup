56062fba11321935eb592b07113822949fa07be1
betty <betty.nabuuso@moringaschool.com>
2021-11-09 11:37:06 +0300

    adding requests

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ class Config:
     NEWS_API_BASE_URL = 'https://newsapi.org/v2/sources?apiKey={}'
     NEWS_API_ARTICLES_URL = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
     NEWS_API_KEY = '0de28d09fb894c7bbc3d053f21fdbc4f'
+    
 
 class ProdConfig(Config):
     pass
